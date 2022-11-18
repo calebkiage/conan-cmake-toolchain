7f2ce6bdb46079e70f857c60f1342619f20c1b9e
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2022-11-18 22:21:33 +0300

    Allow force building specific packages Disable shared libs on windows

--- conanfile.py
+++ conanfile.py
@@ -8,4 +8,13 @@ class HelloConanCmake(ConanFile):
     requires = ["fmt/9.1.0", "libsodium/1.0.18"]
     settings = "os", "compiler", "build_type", "arch"
     generators = ["CMakeDeps", "CMakeToolchain"]
+    
+    def configure(self):
+        # Statically link conan dependencies on Windows.
+        # Conan dependencies have no debug (pdb) symbols. If we want debugger
+        # support, run conan install . --build DEP_YOU_WANT_TO_DEBUG or
+        # conan install . --build * to debug everything.
+        if self.settings.os == "Windows":
+            self.options["*"].shared = False
+
 
