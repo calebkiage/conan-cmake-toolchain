4d71b7a28996b45305cae4bf3bebc5cf827df7fd
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2022-12-08 08:37:24 +0300

    Only run install when conanfile has changed

--- .conan/conan_default_toolchain.cmake
+++ .conan/conan_default_toolchain.cmake
@@ -4,103 +4,147 @@
 find_file(CONAN_FILE_PATH NAMES conanfile.py conanfile.txt PATHS ${CMAKE_SOURCE_DIR} NO_DEFAULT_PATH)
 
 if (NOT CONAN_FILE_PATH)
-    return()
-endif()
+  return()
+endif ()
 
 set(_conan_install_dir "${CMAKE_BINARY_DIR}/conan-install")
 
 function(_get_install_args)
-    set(optionArgs "")
-    set(oneValueArgs "BUILD_TYPE;OUTPUT_VARIABLE")
-    set(miltiValueArgs "")
+  set(optionArgs "")
+  set(oneValueArgs "BUILD_TYPE;OUTPUT_VARIABLE")
+  set(miltiValueArgs "")
 
-    cmake_parse_arguments(_ARG "${optionsArgs}" "${oneValueArgs}" "${miltiValueArgs}" ${ARGV})
+  cmake_parse_arguments(_ARG "${optionsArgs}" "${oneValueArgs}" "${miltiValueArgs}" ${ARGV})
 
-    string(STRIP "${_ARG_BUILD_TYPE}" _build_type)
+  string(STRIP "${_ARG_BUILD_TYPE}" _build_type)
 
-    # Recipes fail if build type is None
-    if(NOT _build_type)
-        message(FATAL_ERROR "Build type should be defined")
-    endif()
+  # Recipes fail if build type is None
+  if (NOT _build_type)
+    message(FATAL_ERROR "Build type should be defined")
+  endif ()
 
-    set(conan_install_args "")
+  set(conan_install_args "")
 
-    # Use new conan cmake generators.
-    list(APPEND conan_install_args install ${CMAKE_SOURCE_DIR} --install-folder ${_conan_install_dir})
+  # Use new conan cmake generators.
+  list(APPEND conan_install_args install ${CMAKE_SOURCE_DIR} --install-folder ${_conan_install_dir})
 
-    if (BUILD_SHARED_LIBS)
-        set(conan_install_args "${conan_install_args};--options:host;build_shared=True")
-    endif()
+  if (BUILD_SHARED_LIBS)
+    set(conan_install_args "${conan_install_args};--options:host;build_shared=True")
+  endif ()
 
-    if (CONAN_FORCE_BUILD_PACKAGES)
-        foreach(_package ${CONAN_FORCE_BUILD_PACKAGES})
-            # Build * if you want to debug
-            list(APPEND conan_install_args --build=${_package})
-        endforeach()
+  if (CONAN_FORCE_BUILD_PACKAGES)
+    foreach (_package ${CONAN_FORCE_BUILD_PACKAGES})
+      # Build * if you want to debug
+      list(APPEND conan_install_args --build=${_package})
+    endforeach ()
+  endif ()
+  list(APPEND conan_install_args --build missing)
+
+  if (CMAKE_GENERATOR)
+    list(APPEND conan_install_args "--conf:host" "tools.cmake.cmaketoolchain:generator=${CMAKE_GENERATOR}")
+  endif ()
+
+  set(machine_types "host;build")
+
+  foreach (type ${machine_types})
+    string(TOLOWER ${type} type_lower)
+    string(TOUPPER ${type} type_upper)
+
+    set(options "profile;settings;conf")
+
+    foreach (op ${options})
+      string(TOLOWER ${op} op_lower)
+      string(TOUPPER ${op} op_upper)
+
+      # Allow setting options for conan in CMake. They use the form: 
+      # CONAN_{HOST|BUILD}_{PROFILE|SETTINGS|CONF}
+      # Example: CONAN_HOST_PROFILE in CMakePresets.json
+      if (CONAN_${type_upper}_${op_upper})
+        list(APPEND conan_install_args --${op_lower}:${type_lower} ${CONAN_${type_upper}_${op_upper}})
+      endif ()
+    endforeach ()
+  endforeach ()
+
+  # Set the build type based on CMake options
+  list(APPEND conan_install_args "--settings:build" "build_type=${_build_type}" "--settings:host" "build_type=${_build_type}")
+
+  # TODO: Is it better to run conan show profile to get the compiler name?
+  if ("${CMAKE_GENERATOR}" MATCHES "Visual Studio.*")
+    if ("${_build_type}" STREQUAL "Debug")
+      set(conan_install_args "${conan_install_args};--settings:build;compiler.runtime_type=Debug")
+      set(conan_install_args "${conan_install_args};--settings:host;compiler.runtime_type=Debug")
     endif ()
-    list(APPEND conan_install_args --build missing)
+  endif ()
 
-    if(CMAKE_GENERATOR)
-        list(APPEND conan_install_args "--conf:host" "tools.cmake.cmaketoolchain:generator=${CMAKE_GENERATOR}")
-    endif()
+  set(${_ARG_OUTPUT_VARIABLE} "${conan_install_args}" PARENT_SCOPE)
+endfunction()
 
-    set(machine_types "host;build")
+function(_get_checksum_file_path file output_var)
+  set(_file_name_var "_file_name")
+  if (${CMAKE_VERSION} GREATER_EQUAL "3.20")
+    cmake_path(GET file FILENAME "${_file_name_var}")
+  else ()
+    get_filename_component("${_file_name_var}" "${file}" NAME)
+  endif ()
 
-    foreach(type ${machine_types})
-        string(TOLOWER ${type} type_lower)
-        string(TOUPPER ${type} type_upper)
+  set("${output_var}" "${_conan_install_dir}/${${_file_name_var}}.sha256.txt" PARENT_SCOPE)
+endfunction()
 
-        set(options "profile;settings;conf")
+function(_verify_checksum file output_var)
+  set("${output_var}" "FALSE" PARENT_SCOPE)
 
-        foreach(op ${options})
-            string(TOLOWER ${op} op_lower)
-            string(TOUPPER ${op} op_upper)
+  _get_checksum_file_path(${file} _file_checksum_path)
 
-            # Allow setting options for conan in CMake. They use the form: 
-            # CONAN_{HOST|BUILD}_{PROFILE|SETTINGS|CONF}
-            # Example: CONAN_HOST_PROFILE in CMakePresets.json
-            if(CONAN_${type_upper}_${op_upper})
-                list(APPEND conan_install_args --${op_lower}:${type_lower} ${CONAN_${type_upper}_${op_upper}})
-            endif()
-        endforeach()
-    endforeach()
+  if (NOT EXISTS "${file}" OR NOT EXISTS "${_file_checksum_path}")
+    return()
+  endif ()
 
-    # Set the build type based on CMake options
-    list(APPEND conan_install_args "--settings:build" "build_type=${_build_type}" "--settings:host" "build_type=${_build_type}")
+  file(SHA256 ${file} _file_sum1)
+  file(READ ${_file_checksum_path} _file_sum2)
 
-    # TODO: Is it better to run conan show profile to get the compiler name?
-    if("${CMAKE_GENERATOR}" MATCHES "Visual Studio.*")
-        if ("${_build_type}" STREQUAL "Debug")
-            set(conan_install_args "${conan_install_args};--settings:build;compiler.runtime_type=Debug")
-            set(conan_install_args "${conan_install_args};--settings:host;compiler.runtime_type=Debug")
-        endif()
-    endif()
+  if ("${_file_sum1}" STREQUAL "${_file_sum2}")
+    set(${output_var} "TRUE" PARENT_SCOPE)
+  endif ()
+endfunction()
 
-    set(${_ARG_OUTPUT_VARIABLE} "${conan_install_args}" PARENT_SCOPE)
+function(_save_checksum file)
+  _get_checksum_file_path(${file} _file_checksum_path)
+  file(SHA256 ${file} _file_sum1)
+  file(WRITE "${_file_checksum_path}" ${_file_sum1})
 endfunction()
 
 function(enable_conan)
-    # Check for conan
-    find_program(CONAN_PATH conan REQUIRED)
+  # Check for conan
+  find_program(CONAN_PATH conan REQUIRED)
 
-    get_property(is_multi GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
-
-    if (is_multi)
-        foreach (type ${CMAKE_CONFIGURATION_TYPES})
-            _get_install_args(BUILD_TYPE "${type}" OUTPUT_VARIABLE conan_install_args)
-            
-            # message("\nCommand: ${CONAN_PATH} ${conan_install_args}\n\n")
-            execute_process(COMMAND ${CONAN_PATH} ${conan_install_args})
-        endforeach ()
-    else ()
-        if(CMAKE_BUILD_TYPE)
-            _get_install_args(BUILD_TYPE "${CMAKE_BUILD_TYPE}" OUTPUT_VARIABLE conan_install_args)
-        else()
-            message(FATAL_ERROR "No CMake build type specified.\nPlease set a build type\ne.g.-DCMAKE_BUILD_TYPE=Release\n")
-        endif()
-        # message("\nCommand: ${CONAN_PATH} ${conan_install_args}\n\n")
-        execute_process(COMMAND ${CONAN_PATH} ${conan_install_args})
+  if (NOT CONAN_FORCE_INSTALL)
+    _verify_checksum(${CONAN_FILE_PATH} _is_valid)
+    if (_is_valid)
+      message(STATUS "Conan file unchanged. Skipping conan install")
+      return()
     endif ()
+  endif ()
+
+  get_property(is_multi GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
+  if (is_multi)
+    foreach (type ${CMAKE_CONFIGURATION_TYPES})
+      _get_install_args(BUILD_TYPE "${type}" OUTPUT_VARIABLE conan_install_args)
+
+      # message("\nCommand: ${CONAN_PATH} ${conan_install_args}\n\n")
+      execute_process(COMMAND ${CONAN_PATH} ${conan_install_args})
+    endforeach ()
+    _save_checksum(${CONAN_FILE_PATH})
+  else ()
+    if (CMAKE_BUILD_TYPE)
+      _get_install_args(BUILD_TYPE "${CMAKE_BUILD_TYPE}" OUTPUT_VARIABLE conan_install_args)
+    else ()
+      message(FATAL_ERROR "No CMake build type specified.\nPlease set a build type\ne.g.-DCMAKE_BUILD_TYPE=Release\n")
+    endif ()
+    # message("\nCommand: ${CONAN_PATH} ${conan_install_args}\n\n")
+    execute_process(COMMAND ${CONAN_PATH} ${conan_install_args})
+    _save_checksum(${CONAN_FILE_PATH})
+  endif ()
 endfunction()
 
 enable_conan()
@@ -109,13 +153,13 @@ set(_toolchain_path "${_conan_install_dir}/conan_toolchain.cmake")
 
 unset(_conan_install_dir)
 
-if(EXISTS "${_toolchain_path}")
-    include("${_toolchain_path}")
-else()
-    set(_error "\nConan generated CMake toolchain file not found.\nSearched location:")
-    file(REAL_PATH "${_toolchain_path}" _real_toolchain EXPAND_TILDE)
-    string(APPEND _error "\n${_real_toolchain}")
-    message(WARNING "${_error}")
-    unset(_error)
-    unset(_real_toolchain)
-endif()
+if (EXISTS "${_toolchain_path}")
+  include("${_toolchain_path}")
+else ()
+  set(_error "\nConan generated CMake toolchain file not found.\nSearched location:")
+  file(REAL_PATH "${_toolchain_path}" _real_toolchain EXPAND_TILDE)
+  string(APPEND _error "\n${_real_toolchain}")
+  message(WARNING "${_error}")
+  unset(_error)
+  unset(_real_toolchain)
+endif ()
